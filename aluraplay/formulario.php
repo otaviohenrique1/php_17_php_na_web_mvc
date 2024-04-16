aa8b3d25619916536efb2f679b0434343936f974
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-04-16 15:48:01 -0300

    commit 4

--- aluraplay/formulario.php
+++ aluraplay/formulario.php
@@ -3,9 +3,10 @@
 $dbPath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO("sqlite:$dbPath");
 $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+
 $video = [
-    'url' => '',
-    'title' => '',
+  'url' => '',
+  'title' => '',
 ];
 
 if ($id !== false && $id !== null) {
@@ -17,41 +18,23 @@ if ($id !== false && $id !== null) {
 
 ?>
 <?php require_once 'inicio-html.php'; ?>
-  <main class="container">
+<main class="container">
 
-    <form
-      class="container__formulario"
-      action="<?= $id === false ? '/novo-video.php' : '/editar-video.php?id=' . $id; ?>"
-      method="post"
-    >
-      <h2 class="formulario__titulo">Envie um vídeo!</h2>
-        <div class="formulario__campo">
-          <label class="campo__etiqueta" for="url">Link embed</label>
-          <input
-            name="url"
-            class="campo__escrita"
-            required
-            placeholder="Por exemplo: https://www.youtube.com/embed/FAY1K2aUg5g"
-            id='url'
-            value="<?= $video['url'] ?>"
-          />
-        </div>
+  <form class="container__formulario" method="post">
+    <h2 class="formulario__titulo">Envie um vídeo!</h2>
+    <div class="formulario__campo">
+      <label class="campo__etiqueta" for="url">Link embed</label>
+      <input name="url" class="campo__escrita" required placeholder="Por exemplo: https://www.youtube.com/embed/FAY1K2aUg5g" id='url' value="<?= $video['url'] ?>" />
+    </div>
 
 
-        <div class="formulario__campo">
-          <label class="campo__etiqueta" for="titulo">Titulo do vídeo</label>
-          <input
-            name="titulo"
-            class="campo__escrita"
-            required
-            placeholder="Neste campo, dê o nome do vídeo"
-            id='titulo'
-            value="<?= $video['title'] ?>"
-          />
-        </div>
+    <div class="formulario__campo">
+      <label class="campo__etiqueta" for="titulo">Titulo do vídeo</label>
+      <input name="titulo" class="campo__escrita" required placeholder="Neste campo, dê o nome do vídeo" id='titulo' value="<?= $video['title'] ?>" />
+    </div>
 
-        <input class="formulario__botao" type="submit" value="Enviar" />
-    </form>
+    <input class="formulario__botao" type="submit" value="Enviar" />
+  </form>
 
-  </main>
+</main>
 <?php require_once 'fim-html.php'; ?>
--- aluraplay/inicio-html.php
+++ aluraplay/inicio-html.php
@@ -22,7 +22,8 @@
       <a class="logo" href="/"></a>
 
       <div class="cabecalho__icones">
-        <a href="/pages/enviar-video.html" class="cabecalho__videos"></a>
+        <a href="/novo-video" class="cabecalho__videos"></a>
+        <!-- <a href="/pages/enviar-video.html" class="cabecalho__videos"></a> -->
         <a href="./pages/login.html" class="cabecalho__sair">Sair</a>
       </div>
     </nav>
