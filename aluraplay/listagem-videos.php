a0af942f734b152f392b27448bd820672f8cc131
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-04-16 21:18:22 -0300

    commit 7

--- aluraplay/listagem-videos.php
+++ aluraplay/listagem-videos.php
@@ -13,11 +13,6 @@ $videoList = $repository->all();
 <?php require_once 'inicio-html.php'; ?>
 <ul class="videos__container" alt="videos alura">
   <?php foreach ($videoList as $video) : ?>
-    <!-- <?php
-    // if (!str_starts_with($video['url'], 'http')) {
-    //   $video['url'] = 'https://www.youtube.com/embed/CXz54XNvd98?si=7r1GD0n1TN7YK22t';
-    // }
-    ?> -->
     <li class="videos__item">
       <iframe width="100%" height="72%" src="<?= $video->url; ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <div class="descricao-video">
